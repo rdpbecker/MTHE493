84103261d2f8453472cefcbf60cedd3e2bc8e98d
Riley <rileydbecker@gmail.com>
2018-11-26 23:14:12 -0500

    Add code to write randomly generated data to a file

--- kMeans/src/fileio.py
+++ kMeans/src/fileio.py
@@ -1,4 +1,4 @@
-import csv
+import csv,random as rand
 
 def readFile(filepath):
     points = []
@@ -11,4 +11,25 @@ def readFile(filepath):
             points.append(current)
     return points
 
-print readFile("../Testing/test0.csv")
+def writeFile(filepath,length,n):
+    points = []
+    for i in range(n):
+        theSum = 0
+        point = []
+        for j in range(length-1):
+            num = rand.uniform(0,1-theSum)
+            theSum = theSum + num
+            point.append(num)
+        point.append(1-theSum)
+        points.append([str(x) for x in point])
+    
+    with open(filepath,'w') as csvfile:
+        csvwriter = csv.writer(csvfile)
+        for point in points:
+            csvwriter.writerow(point)
+    return points
+
+if __name__ == "__main__":
+    import sys
+    args = sys.argv[1:]
+    writeFile("../Testing/test"+args[0]+".csv",int(args[1]),int(args[2]))
