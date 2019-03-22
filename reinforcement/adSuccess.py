5a602c062a8ab768625a8338c87d38fcec09fcdd
Riley <rileydbecker@gmail.com>
2019-03-22 18:26:42 -0400

    Add a timer and fix up the people

--- reinforcement/adSuccess.py
+++ reinforcement/adSuccess.py
@@ -1,4 +1,5 @@
 import person as p, random, integrate as inte 
+from timeit import default_timer as timer
 
 def giveRandom(people,iters,adsList):
     successes = 0
@@ -9,12 +10,25 @@ def giveRandom(people,iters,adsList):
 
 def giveTargetted(people,iters):
     successes = 0
-    for i in range(iters):
-        for j in range(len(people)):
-            print people[j].getProbsEmpirical()
-            probs = inte.findProbs(people[j].getProbsEmpirical(),people[j].confidence())
-            maxIndex = people[j].getMaxIndexActual()
-            successes = successes + people[j].clickAd(maxIndex)
+    intTime = 0
+    adTime = 0
+    start = timer()
+    for i in range(len(people)):
+#       start = timer()
+        probs = inte.findProbs(people[i].getProbsEmpirical(),people[i].confidence(),0)
+#       mid = timer()
+        for j in range(iters):
+#           intTime = intTime + mid - start
+            maxIndex = people[i].getMaxIndexActual()
+            successes = successes + people[i].clickAd(maxIndex)
+#       end = timer()
+#       adTime = adTime + end - mid
+        if i and not i%100:
+            print "Done person ", i, " targetted"
+#       print "Integration time: ", intTime
+#       print "Ad distribution time: ", adTime
+#       print "% integration time: ", intTime/(intTime+adTime)
+#       print ""
     return float(successes)/len(people)/iters
 
 def oneIter(n,num,searches,adsGiven):
@@ -24,6 +38,8 @@ def oneIter(n,num,searches,adsGiven):
         people.append(p.Person(n))
         for j in range(searches):
             people[i].randomSearch()
+#       print "Generated person ", i
+#       people[i].printPerson()
     randPct = giveRandom(people,adsGiven,adsList)
     targetPct = giveTargetted(people,adsGiven)
     print "With random ads: ", randPct
@@ -42,7 +58,17 @@ def main(dim,people,searches,adsGiven,peopleSets):
     print "With random ads: ", randSum/peopleSets
     print "With targetted ads: ", targetSum/peopleSets
 
+def testUpdating():
+    person = p.Person(3)
+    person2 = p.Person(3)
+    for i in range(5):
+        person.randomSearch()
+        person2.randomSearch()
+    person.printPerson()
+    person2.printPerson()
+
 if __name__ == "__main__":
     import sys
     args = sys.argv[1:]
     main(int(args[0]),int(args[1]),int(args[2]),int(args[3]),int(args[4]))
+#   testUpdating()
