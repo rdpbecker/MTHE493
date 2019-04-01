fbf165ac427c0effe053f91d2d7682f4da96282f
Riley <rileydbecker@gmail.com>
2019-04-01 15:00:30 -0400

    Comment learn and change path dependencies

--- reinforcement/two/learn.py
+++ reinforcement/two/learn.py
@@ -1,6 +1,18 @@
 import random, sys
-sys.path.append("../People")
+sys.path.append("../../People")
 from two import person as p
+
+##############################################################
+## Print a dictionary
+##
+## Parameters: aDict - the dictionary to print
+##             header - the header to printed at the start of 
+##                      the dictionary
+##             elemHeader - a header to print before each 
+##                          element
+##
+## Returns: None
+##############################################################
 
 def printDict(aDict,header,elemHeader):
     print header
@@ -8,11 +20,24 @@ def printDict(aDict,header,elemHeader):
         print elemHeader, " ", i, ": ", aDict[i]
     print ""
 
+##############################################################
+## Chooses a random integer between 0 and n-1
+##############################################################
+
 def randInt(n):
     randNum = random.uniform(0,n)
     for i in range(n):
         if randNum >= i and randNum <= (i+1):
             return i
+
+##############################################################
+## Finds the index of the maximum element in an array
+##
+## Parameters: percents - the array to find the maximum of
+##             n - the length of percents
+##
+## Returns: the index of the maximum element in percents
+##############################################################
 
 def maxIndex(percents,n):
     maxIndex = 0
@@ -20,6 +45,17 @@ def maxIndex(percents,n):
         if percents[i] > percents[maxIndex]:
             maxIndex = i
     return maxIndex 
+
+##############################################################
+## Learns the best ad to present a person using a stochastic
+## policy
+##
+## Parameters: person - the person whose best ad we should 
+##                      learn
+##             decay - the decay rate of the policy
+##
+## Returns: None
+##############################################################
 
 def learn(person,decay):
     clicks = {}
@@ -46,16 +82,18 @@ def learn(person,decay):
     printDict(ads,"Number of times each ad is selected", "Ad selected")
     printDict(percents,"Percents","Percent for ad")
 
+##############################################################
+## Generates a person and learns the best ad to give them
+##############################################################
+
 def main():
     person = p.Person(3)
-
-    count = 0
-    for i in range(100):
-         if person.clickAd(2):
-            count = count + 1
-
     learn(person,0.98)
     person.printProbs()
+
+##############################################################
+## Runs the script
+##############################################################
 
 if __name__ == "__main__":
     main()
